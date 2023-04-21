d3d9cad31a694a8fcd78bd081f49aaa7e042737e
Chido Le Riz <99027192+Charl2fde@users.noreply.github.com>
2023-04-21 16:27:25 +0200

    connexion

--- php/connexion.php
+++ php/connexion.php
@@ -1,11 +1,13 @@
 <?php
 
-// Paramètres de connexion
-$host = 'localhost';
-$dbname = 'id20407534_legendary';
+// Connexion à la base de données
+$host = "localhost";
+$dbname = "id20605725_legendary";
+$username = "id20605725_charles";
+$password = "Chatelard0300$"; 
 // Connexion à la base de données
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", "root", "");
+    $db = new PDO("mysql:host=$host;dbname=$dbname", "id20605725_charles", "Chatelard0300$");
     // Configuration des options de PDO
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
