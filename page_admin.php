333501ddb40577096629652518c9e28e8cb5fc7c
MrPropreuuh <vincent.fougere@epsi.fr>
2023-06-03 15:02:20 +0200

    commit

--- page_admin.php
+++ page_admin.php
@@ -1,6 +1,26 @@
 <?php
 include "connexion.php";
+session_start();
+if (!isset($_SESSION['email']) || $_SESSION['role'] !== 'admin') {
+    // Rediriger vers une page d'erreur ou de connexion
+    header("Location: page_erreur.php");
+    exit;
+}
 
+// Vérifier si le bouton de déconnexion a été cliqué
+if (isset($_POST['deconnexion'])) {
+    // Supprimer toutes les variables de session
+    session_unset();
+
+    // Détruire la session
+    session_destroy();
+
+    // Rediriger vers la page de connexion ou une autre page de votre choix
+    header("Location: index.php");
+    exit;
+}
+?>
+<?php
 // Vérifier si la connexion à la base de données est établie avec succès
 if ($db) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -42,10 +62,11 @@ if ($db) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>Legendary Motorsport</title>
 </head>
 
 <body>
@@ -116,7 +137,11 @@ if ($db) {
                     <button id="bouton-supprimer" name="bouton-supprimer" value="supprimer"><i class="fas fa-trash-alt"></i> Mettre le stock a 0</button>
                 </div>
             </section>
+
         </div>
+        <form method="POST">
+    <button type="submit" name="deconnexion">Se déconnecter</button>
+</form>
     </form>
     <script>
         function updateMotoInfo() {
