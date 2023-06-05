c47044fbbc9f616a1c79ba3500e49b42a22401b3
MrPropreuuh <vincent.fougere@epsi.fr>
2023-06-05 16:06:25 +0200

    Bdd code smell

--- connexion.php
+++ connexion.php
@@ -3,11 +3,11 @@
 // Connexion à la base de données
 $host = "localhost";
 $dbname = "id20605725_legendary";
-$username = "root";
-$password = ""; 
+$username = "id20605725_charles";
+$password = ";4VD}j,N>t/2AUm7SC"; 
 // Connexion à la base de données
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", "root", "");
+    $db = new PDO("mysql:host=$host;dbname=$dbname", "id20605725_charles", ";4VD}j,N>t/2AUm7SC");
     // Configuration des options de PDO
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
