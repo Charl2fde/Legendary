27a097b7caa839acffeab990bd85afa6ab6ecc26
MrPropreuuh <vincent.fougere@epsi.fr>
2023-06-05 16:27:45 +0200

    config issue vulnerability

--- connexion.php
+++ connexion.php
@@ -1,18 +1,17 @@
 <?php
 
-// Connexion à la base de données
-$host = "localhost";
-$dbname = "id20605725_legendary";
-$username = "id20605725_charles";
-$password = ";4VD}j,N>t/2AUm7SC"; 
+// Inclure le fichier de configuration
+include "./config/config.php";
+
 // Connexion à la base de données
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", "id20605725_charles", ";4VD}j,N>t/2AUm7SC");
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // Configuration des options de PDO
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
+
 
 ?>
